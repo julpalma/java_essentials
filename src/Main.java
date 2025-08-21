14f1f043c94fbcee2315d8c12a216a436582ad0a
julpalma <jufpalma@yahoo.com.br>
2025-08-21 13:23:53 -0500

    commit for java essentials - small enhancements

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,9 @@ import polymorphism.Animal;
 import polymorphism.Cat;
 import polymorphism.Dog;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         System.out.println("Hello and welcome!!!!");
 
         // -------- Instantiate Object Rectangle --------
